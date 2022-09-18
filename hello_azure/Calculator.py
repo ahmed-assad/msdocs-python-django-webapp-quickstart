4a19bd91c0719f515f58cce390eb26ff441245e9
ahmed-assad <49638344+ahmed-assad@users.noreply.github.com>
2022-09-18 14:20:57 +0200

    firstTestCase

---
+++ hello_azure/Calculator.py
@@ -0,0 +1,5 @@
+class Calculator:
+    def Calc(num1,num2):
+        return num1+num2
+
+    
--- hello_azure/tests.py
+++ hello_azure/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from . import Calculator
+
+class CalculatorTestCase(TestCase):
+    def setUp(self):
+        # self.assertIn(5,Calculator.Calculator.Calc(num1=2,num2=3))
+        Calculator.Calculator.Calc(num1=2,num2=3)
+        Calculator.Calculator.Calc(num1=3,num2=4)
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        test1 = Calculator.Calculator.Calc(num1=2,num2=3)
+        test2 = Calculator.Calculator.Calc(num1=2,num2=3)
+        self.assertEqual(test1, 5)
+        self.assertEqual(test2, 5)
 
 # Create your tests here.
